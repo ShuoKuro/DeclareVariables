c48075329a058c83ef581c746cd6438224092530
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 01:14:29 +0800

    fix space

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,9 @@ fn main() {
         "{},{},{},{},{},{}",
         arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]
     );
-    let persons: (&str, i8, &str, i8, &str, i8) = ("Alex", 21, " Abe", 22, " Anna", 23);
+    let persons: (&str, i8, &str, i8, &str, i8) = ("Alex", 21, "Abe", 22, "Anna", 23);
     print!(
-        "{}:{},{}:{},{}:{}",
+        "{}:{}, {}:{}, {}:{}",
         persons.0, persons.1, persons.2, persons.3, persons.4, persons.5
     );
 }
