598c1622b72765c3f1e39b5f2c8e77be0128890a
DESKTOP-B5IRF34\shuoya <timehet556947@gmail.com>
2023-05-01 01:13:50 +0800

    done

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
     let arr: [i8; 6] = [0, 2, 4, 6, 8, 10];
-    println!(
+    print!(
         "{},{},{},{},{},{}",
         arr[0], arr[1], arr[2], arr[3], arr[4], arr[5]
+    );
+    let persons: (&str, i8, &str, i8, &str, i8) = ("Alex", 21, " Abe", 22, " Anna", 23);
+    print!(
+        "{}:{},{}:{},{}:{}",
+        persons.0, persons.1, persons.2, persons.3, persons.4, persons.5
     );
 }
